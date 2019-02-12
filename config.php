3b318e8e2033d8cbe3cdca5e0ffc19ade17d29c6
shadowsnhd <pxsniper112@gmail.com>
2019-02-12 08:42:48 -0500

    little bit of changes

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $config = array();
 
 // a unique key that identifies this application - DO NOT LEAVE THIS EMPTY!
-$config['app_key'] = 'titaniumnetwork1241';
+$config['app_key'] = 'WelliwasshoppingforanewcarwhichonesmeAcoolconvertibleoranSUVToobadididntknowmycreditwaswhackandnowImdrivingoffthelotinausedsubcompactFREEthatspellsfreecreditreportdotcombabySawtheiradsonmyTVthoughtaboutgoingbutwastoolazyNowinsteadoflookingflyandrollingfatMylegsarestickingtothevinylandmypossesgettinglaughedatFREEthatspellfreecreditreportdotcombaby';
 
 // a secret key to be used during encryption
 $config['encryption_key'] = '';
