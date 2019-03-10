42e79dcf103e4513b25ba3e18fc3973601959be8
shadowsnhd <pxsniper112@gmail.com>
2019-03-10 18:02:17 -0400

    putting back a CSS

--- ytdl/templates/header.php
+++ ytdl/templates/header.php
@@ -8,6 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link href="../css/bootstrap.min.css" rel="stylesheet">
 		<link href="../css/primary.css" rel="stylesheet">
+		<link href="./css/custom.css" rel="stylesheet">
 </head>
 <body>
 	<nav class="navbar white-text navbar-expand-lg navbar-dark fixed-top" id="navbar">
