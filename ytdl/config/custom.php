500abf6ba9d5ec3fbdbf58220aad990bc1822eb6
shadowsnhd <pxsniper112@gmail.com>
2019-03-10 18:48:57 -0400

    configs

--- ytdl/config/custom.php
+++ ytdl/config/custom.php
@@ -2,4 +2,8 @@
 // in config/custom.php
 return [
     'enable_youtube_decipher_signature' => true,
+        'localCache' => true,
+        'MP3Enable' => true,
+        'MP3ConvertVideo' => true,
+        'MP3Quality' => '320k',
 ];
