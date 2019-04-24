3d87521f9199f155f82c98a0bfd323d2630c5916
shadowsnhd <pxsniper112@gmail.com>
2019-04-24 08:49:26 -0400

    Removing NodeUnblocker, Fixing Cookie Clicker, and New Wallpaper

--- nodefiles/host.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var URL = require('url');
-
-module.exports = function( /*config*/ ) {
-    return function hostHeader(data) {
-        data.headers.host = URL.parse(data.url).host;
-    };
-};
