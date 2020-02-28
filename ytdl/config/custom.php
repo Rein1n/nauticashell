94737eb6823bed082d2bf04c40529fd59cf1ce07
LQ16 <59111554+LQ16@users.noreply.github.com>
2020-02-28 18:39:38 -0500

    big update

--- ytdl/config/custom.php
+++
@@ -1,9 +0,0 @@
-<?php
-// in config/custom.php
-return [
-    'enable_youtube_decipher_signature' => true,
-        'localCache' => true,
-        'MP3Enable' => true,
-        'MP3ConvertVideo' => true,
-        'MP3Quality' => '320k',
-];
